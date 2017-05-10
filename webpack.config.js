3c37a4e7f6e70ad1ba9e1af3ba25e9f745f22a6f
Franco Martinelli <f.martinelli@nrgsys.it>
2017-05-10 13:07:24 +0200

    Minimal repo for showing error

---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+const path = require('path');
+
+const config = {
+    entry: "./src/Index.tsx",
+    output: {
+        filename: "bundle.js",
+        path: path.join(__dirname, "dist")
+    },
+
+    // Enable sourcemaps for debugging webpack's output.
+    devtool: "source-map",
+
+    resolve: {
+        // Add '.ts' and '.tsx' as resolvable extensions.
+        extensions: ['.ts', '.tsx', '.js', '.jsx']
+    },
+
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                loader: 'ts-loader'
+            },
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader']
+            }
+        ]
+    },
+
+    devServer: {
+        contentBase: path.join(__dirname, "dist")
+    }
+};
+
+module.exports = config;
